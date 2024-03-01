593f925377c149a50b73f3d18cd6d42c233cd5b5
Abhishek <abhiprasad04@gmail.com>
2024-03-01 08:14:51 +0530

    code updated8

--- src/unittest/python/test_webapp.py
+++ src/unittest/python/test_webapp.py
@@ -3,10 +3,12 @@ import pytest
 
 from app import app
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+
 
 def test_index_page(client):
     response = client.get('/')
